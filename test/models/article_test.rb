6cad74b40e9f5879941bd10a3ce4cdb566399ea9
Sean Collins <sean@cllns.com>
2015-12-29 22:55:38 -0700

    Add test for validating length of article title

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "doesn't allow title of less than 5 character" do
+    article = Article.new(title: "abcd")
+    assert !article.valid?
+  end
+
+  test "allow title of 5 characters" do
+    article = Article.new(title: "abcde")
+    assert article.valid?
+  end
 end
