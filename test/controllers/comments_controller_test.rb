1c3b8b3abd661bc526b8cad28a54eabb3634fc75
Sean Collins <sean@cllns.com>
2015-12-30 18:36:33 -0700

    Add tests for CommentsController

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @comment = comments(:one)
+    request.headers['Authorization'] = ActionController::HttpAuthentication::Basic.
+      encode_credentials('dhh', 'secret')
+  end
+
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post :create, article_id: @comment.article.id, comment: { body: 'Great article' }
+    end
+
+    assert_redirected_to article_path(@comment.article)
+  end
+
+  test "should destroy comment" do
+    assert_difference('Comment.count', -1) do
+      delete :destroy, article_id: @comment.article.id, id: @comment
+    end
+
+    assert_redirected_to article_path(@comment.article)
+  end
 end
